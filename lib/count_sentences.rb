4b9bad4da7bee7d07b6fc10504162ab9537e6a1a
Bruce Parker <detailsharks@gmail.com>
2020-06-14 14:44:01 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,7 +17,8 @@ class String
   def count_sentences
     self.split(/[.!?]/).reject {|x| x.empty?}.size
     
-    
+      def count_sentences_words
+    self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size 
 
   end
 end
